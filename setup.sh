478a0bd6bdfa99c41b7f81fe93d640b1b30498eb
sneekyfoxx <sneekyfoxx09@gmail.com>
2024-07-29 18:34:08 -0400

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+
+setup() {
+  local prefix="$1";
+  local curdir="$(pwd)";
+  local py_ver="$(ls /usr/bin | grep -E '^python[0-9][.][0-9]+$' | tr '\n' ' ' | cut -d ' ' -f1)";
+  local pattern="^[/]";
+
+  if [ ! "${prefix}" ] || [ ! -d "${prefix}" ]; then
+    prefix="${HOME}/.local/bin";
+
+    echo -e "Creating ziggy python zipfile\n";
+    sleep 1;
+    command "${py_ver}" -m zipapp -o ziggy -p "/usr/bin/env python3" -c "${curdir}/pkg";
+
+    echo -e "Moving ziggy to ${prefix}";
+    sleep 1;
+    mv "${curdir}/ziggy" "${prefix}/ziggy";
+  else
+    echo -e "Creating ziggy python zipfile\n";
+    sleep 1;
+    command "${py_ver}" -m zipapp -o ziggy -p "/usr/bin/env python3" -c "${curdir}/pkg";
+
+    echo -e "Moving ziggy to ${prefix}";
+    sleep 1;
+    if [[ $prefix =~ $pattern ]]; then
+      echo -e "sudo privileges is need to move 'ziggy' to '${prefix}'";
+      sudo mv "${curdir}/ziggy" "${prefix}/ziggy";
+    else
+      mv "${curdir}/ziggy" "${prefix}/ziggy";
+    fi
+  fi
+}
+
+setup "$@";
+ziggy;
